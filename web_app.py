ba77b89d0f7603d6c3a37952b3149f33b1f21277
ItayLev <itay.leb@gmail.com>
2026-05-24 21:00:35 +0300

    Added features to Add/Delete airplanes and commit changes to data-base

--- web_app.py
+++ web_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from helpers import get_dummy_data, set_dummy_data, update_net_req
+from helpers import get_data_base, set_data_base
 
 app = Flask(__name__)
 
@@ -29,6 +30,50 @@ def update():
     update_net_req()
 
     return redirect("/")
+
+
+# -------- ADD MODEL --------
+@app.route("/add_model", methods=["POST"])
+def add_model():
+    name = request.form["model"]
+
+    data = get_dummy_data()
+
+    new_model = {
+        "model": name,
+        "id": 999999,
+        "systems": {
+            "engine": {"available": 0, "gross": 0, "net-req": 0},
+            "air-frame": {"available": 0, "gross": 0, "net-req": 0},
+        }
+    }
+
+    data.append(new_model)
+    set_dummy_data(data)
+
+    return redirect("/")
+
+
+# -------- DELETE MODEL --------
+@app.route("/delete/<model>")
+def delete(model):
+    data = get_dummy_data()
+
+    data = [m for m in data if m["model"] != model]
+
+    set_dummy_data(data)
+
+    return redirect("/")
+
+
+# -------- COMMIT CHANGES --------
+@app.route("/commit")
+def commit():
+    data = get_dummy_data()
+    set_data_base(data)
+
+    return redirect("/")
+
     
 # -------- START SERVER --------
 if __name__ == "__main__":

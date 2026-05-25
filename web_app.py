9f352473fbac51d64fa0ef5ee821f0a485d35c2c
ItayLev <itay.leb@gmail.com>
2026-05-25 20:54:57 +0300

    fixed bugs on add-new-airplane + delete-airplane + initialize data

--- web_app.py
+++ web_app.py
@@ -3,11 +3,14 @@ from helpers import get_dummy_data, set_dummy_data, update_net_req
 from helpers import get_data_base, set_data_base
 
 app = Flask(__name__)
+initial_data = get_data_base()
+set_dummy_data(initial_data)
 
 # -------- HOME PAGE --------
 @app.route("/")
 def home():
     data = get_dummy_data()
+    print(data)
     return render_template("index.html", data=data)
 
 # -------- UPDATE SYSTEM --------
@@ -15,16 +18,22 @@ def home():
 def update():
     model = request.form["model"]
     system = request.form["system"]
-    available = int(request.form["available"])
-    gross = int(request.form["gross"])
-
+    available = request.form["available"]
+    gross = request.form["gross"]
     data = get_dummy_data()
 
     for m in data:
         if m["model"] == model:
             if system in m["systems"]:
-                m["systems"][system]["available"] = available
-                m["systems"][system]["gross"] = gross
+                if available or gross:
+                    if available:
+                        m["systems"][system]["available"] = int(available)
+                    else:
+                        continue
+                    if gross:
+                        m["systems"][system]["gross"] = int(gross)
+                    else:
+                        continue
 
     set_dummy_data(data)
     update_net_req()
@@ -36,15 +45,29 @@ def update():
 @app.route("/add_model", methods=["POST"])
 def add_model():
     name = request.form["model"]
+    print("DEBUG: adding model:", name)
 
     data = get_dummy_data()
 
     new_model = {
         "model": name,
         "id": 999999,
+        "systems-list": [
+            "engine", "air-frame", "avionics",
+            "flight-controls", "systems"
+        ],
         "systems": {
             "engine": {"available": 0, "gross": 0, "net-req": 0},
             "air-frame": {"available": 0, "gross": 0, "net-req": 0},
+            "avionics": {"available": 0, "gross": 0, "net-req": 0},
+            "flight-controls": {"available": 0, "gross": 0, "net-req": 0},
+
+            "peripheral-systems": {
+                "landing-gear": {"available": 0, "gross": 0, "net-req": 0},
+                "fuel-system": {"available": 0, "gross": 0, "net-req": 0},
+                "hydraulic-system": {"available": 0, "gross": 0, "net-req": 0},
+                "electrical-system": {"available": 0, "gross": 0, "net-req": 0}
+            }
         }
     }
 
